501eb4efb4fe7b655551c8d7562c860e5662b0e9
OVAN <giovannimahendra55@gmail.com>
2025-12-23 18:44:22 +0700

    fress

--- Test.cpp/test.cpp
+++ Test.cpp/test.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <map>
+using namespace std;
 
 int main() {
-    std::cout << "woi kok gak bisaaaa" << std::endl;
+    map<string, int> votes;
+    int totalVoters;
+    
+    cout << "=== SISTEM VOTING MEREK MOBIL ===" << endl;
+    cout << "Masukkan jumlah pemilih: ";
+    cin >> totalVoters;
+    cin.ignore();
+    
+    for (int i = 1; i <= totalVoters; i++) {
+        string brand;
+        cout << "\nPemilih ke-" << i << ", pilih merek mobil: ";
+        getline(cin, brand);
+        votes[brand]++;
+    }
+    
+    cout << "\n=== HASIL VOTING ===" << endl;
+    for (auto& vote : votes) {
+        cout << vote.first << ": " << vote.second << " suara" << endl;
+    }
+    
     return 0;
 }
